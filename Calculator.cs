41e6704750f1a69362e4de9dd194848d606f797c
Nanta <listianta.adhe.n@students.amikom.ac.id>
2025-03-13 10:07:46 +0700

    kurang

--- Calculator.cs
+++ Calculator.cs
@@ -6,6 +6,10 @@ class Calculator
 		int b = 6;
 		
 		Console.Writeline("Hasil Penambahan: {0} +{1} = {2}", a, b, Penambahan(a,b));
+		Console.Writeline("Hasil Pengurangan: {0} +{1} = {2}", a, b, Pengurangan(a,b));
+		
+		Console.Writeline("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	static int Penambahan(int a, int b)
 	{
