eb7384152cc7e4081d90f4e620d2339f07ff7702
Nanta <listianta.adhe.n@students.amikom.ac.id>
2025-03-13 10:02:33 +0700

    pembaruan

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+class Calculator
+{
+	static void Main(string[]args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline("Hasil Penambahan: {0} +{1} = {2}", a, b, Penambahan(a,b));
+	}
+	static int Penambahan(int a, int b)
+	{
+		return a+b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a-b;
+	}
+}
